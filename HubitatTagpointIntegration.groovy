757189617b1503dc5b805000d9aecc0c4c848934
IgorKostyukov <119662895+IgorKostyukov@users.noreply.github.com>
2023-11-01 19:20:57 +0300

    Add files via upload

---
+++ HubitatTagpointIntegration.groovy
@@ -0,0 +1,224 @@
+import groovy.transform.Field
+
+@Field deleteRulePrefix = "deleteRule"
+
+@Field gtcomp = "greater than"
+@Field ltcomp = "less than"
+
+
+definition(
+    name: "TagpointAlerts",
+    namespace: "waveaccess",
+    author: "IgorKostyukov",
+    description: "",
+    category: "",
+    iconUrl: "",
+    iconX2Url: "")
+
+
+preferences {
+    page(name: "mainPage", title: "Main Page", install: true, uninstall: true) {
+        section() {
+            input "authHeader", "text", title: "Auth Header", multiple: false, required: true, submitOnChange: true    
+            href name: "myHref", "button", page: "addRulePage", title: "Add new Rule"
+        }        
+       
+        section("Existing Rules") {
+            def rulesText
+            state.subscriptions.each { subscriptionId, subscription ->
+                subscription.each { ruleId, ruleData ->
+                    rulesText = ""
+                    rulesText += "Rule ID: $ruleId\n"
+                    rulesText += "Name: ${ruleData.name}\n"
+                    rulesText += "Parameter Type: ${ruleData.parameterType}\n"
+                    rulesText += "Device: ${ruleData.device.displayName}\n"
+                    rulesText += "Comparison: ${ruleData.comparison}\n"
+                    rulesText += "Value: ${ruleData.value}\n"
+                    rulesText += "URL: ${ruleData.url}\n"                                
+                    paragraph(rulesText)                
+                    input "${deleteRulePrefix}_${subscriptionId}_${ruleId}", "button", title: "Delete"
+                    paragraph("-------------------\n")                    
+                }
+            }
+        }
+    }
+    
+       
+    page(name: "addRulePage", title: "Add new rule", install: false, uninstall: true) {
+      dynamicPage(name: "addRulePage") {
+          section("") {
+              input "name", "text", title: "Name", multiple: false, required: true, submitOnChange: true    
+              if (name) {
+                  input "parameterType", "enum", title: "Parameter Type", options: ["temperature", "motion", "humidity"], submitOnChange: true   
+              }             
+              if (parameterType) {
+                  input "device", getCapability(parameterType), title: "Device", multiple: false, required: true, submitOnChange: true    
+              }
+              if (device) {
+                  input "comparison", "enum", title: "Comparison", options: [gtcomp, ltcomp], submitOnChange: true
+              }
+              if (comparison) {
+                    input "value", "decimal", title: "Value", required: true, submitOnChange: true
+              }            
+              if (value) {
+                    input "recheck", "number", title: "Recheck", required: true, submitOnChange: true
+              }            
+              if (recheck) {
+                  input "url", "text", title: "URL", required: true, submitOnChange: true
+              }              
+              if (url) {
+                  input "create", "button", title: "Add"
+              }              
+          }          
+     }
+   } 
+}
+
+def createSubscriptionKey(device, parameterType) {
+    return "${device.id}${parameterType}"
+}
+
+def recheckHandler(subscriptionKey, ruleId) {
+    log.debug "rule is enabled"
+    def rule = findRule(subscriptionKey, ruleId)
+    if (rule) {
+        rule.enabled = true
+    }
+}
+
+def eventHandler(event) {   
+    log.debug "eventHandler"
+    def subscriptionKey = createSubscriptionKey(event.getDevice(), event.name)
+    def subscription = state.subscriptions[subscriptionKey]
+    if (subscription) {     
+        log.debug "subscription"
+        subscription.each { ruleId, rule ->
+            if (rule.enabled) {
+                log.debug "rule"
+                if ((rule.comparison == gtcomp && event.getDoubleValue() > rule.value) || (rule.comparison == ltcomp && event.getDoubleValue() < rule.value)) {
+                    log.debug "post"                    
+                    Map postParams = [
+                        uri: rule.url,
+                        requestContentType: 'application/json',
+                        contentType: 'application/json',
+                        headers: ['Auth': authHeader],
+                        body : new groovy.json.JsonOutput().toJson( [ value: event.value ])
+                    ]
+   
+                    /*
+                    httpPost(postParams) { resp ->
+                        log.debug "tagpoint respStatus: ${resp?.getStatus()}"
+                    } */                  
+                    
+                    log.debug "recheck - ${rule.recheck}"
+                    if (rule.recheck >= 0) {
+                        rule.enabled = false
+                        log.debug "runIn"
+                        runIn(rule.recheck*60, recheckHandler, [overwrite: false, data: [subscriptionKey, ruleId]])
+                    }
+                }            
+            }
+            else {
+                log.debug "rule is disabled"
+            }    
+        }
+    }
+    else {
+        log.debug "usubscribe ${event.device.displayName} ${event.name}"
+        unsubscribe(event.device, event.name)        
+    }        
+}    
+
+def findRule(subscriptionId, ruleId) {
+    def s = state.subscriptions[subscriptionId]
+    if (s) {
+        return s[ruleId]
+    }
+    return null
+}
+
+void appButtonHandler(btn) {
+    log.debug "Button pushed - ${btn}"   
+    if (btn == "create") { 
+        def newRuleData = [
+            name: name,
+            parameterType: parameterType,
+            device: device,
+            comparison: comparison,
+            value: value,
+            recheck: recheck,
+            url: url,
+            enabled: true
+        ]        
+       
+        def ruleId = new Date().time.toString()
+        
+        if (!state.subscriptions) {
+            state.subscriptions = [:]
+        }
+        
+        def subscriptionKey = createSubscriptionKey(newRuleData.device, newRuleData.parameterType)
+                
+        if (!state.subscriptions) {
+            state.subscriptions = [:]
+        }
+        
+        if (!state.subscriptions[subscriptionKey]) {
+            state.subscriptions[subscriptionKey] = [:]            
+        }        
+        
+        if (state.subscriptions[subscriptionKey].isEmpty()) {
+            log.debug "subscribe - ${subscriptionKey}"
+            subscribe(newRuleData.device, newRuleData.parameterType, eventHandler, ["filterEvents": false])            
+        }
+        
+        state.subscriptions[subscriptionKey][ruleId] = newRuleData    
+    }
+    else if (btn.startsWith(deleteRulePrefix)) {
+        def substrings = btn.tokenize("_");
+        def subscriptionKey = substrings[1]
+        def ruleId = substrings[2]
+        def rule = findRule(subscriptionKey, ruleId)
+        if (rule) {            
+            def subscription = state.subscriptions[subscriptionKey]
+            if (subscription) {
+                def sr = subscription[ruleId]
+                if (sr) {
+                    subscription.remove(ruleId)
+                    if (subscription.size() == 0) {
+                        state.subscriptions.remove(subscriptionKey)
+                    }                    
+                }
+            }
+        }
+    }        
+}
+
+def getCapability(parameterType) {
+    if (parameterType == "temperature") {
+        return "capability.temperatureMeasurement"
+    }
+    else if (parameterType == "humidity") {
+        return "capability.relativeHumidityMeasurement"
+    }
+    else if (parameterType == "motion") {
+        return "capability.motionSensor"
+    }
+    else {
+        return ""   
+    }
+}    
+
+def installed() {
+    log.debug "installed"    
+    updated()
+}
+
+def updated() {
+    log.debug "updated"    
+}
+
+def uninstalled() {
+    unsubscribe()
+    log.debug "uninstalled"
+}
